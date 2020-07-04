cba4a68f5d121692c0d0718d1cc88be47b2ebf67
Henrique Miranda <enrque.ns@gmail.com>
2020-07-04 19:06:02 -0300

    :lipstick: style: custom card on cart

--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -8,18 +8,50 @@ export const Container = styled.div`
 `;
 
 export const PokemonList = styled.ul`
+  margin-top: 128px;
+
   li {
     display: flex;
     align-items: center;
     justify-content: space-between;
 
+    font-size: 2rem;
+    color: ${({ theme }) => theme.colors.active};
+
     border: 2px solid ${({ theme }) => theme.colors.secondaryText};
     border-radius: 4px;
 
-    padding: 24px;
+    padding: 24px 24px 16px;
 
     img {
       height: 96px;
+
+      margin-top: -40px;
+      margin-left: -16px;
+    }
+
+    p {
+      flex: 1;
+      text-transform: capitalize;
+
+      margin: -8px 24px 0;
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      margin-top: -8px;
+
+      span {
+        margin: 0 24px;
+      }
+
+      button {
+        width: 24px;
+        height: 24px;
+        font-size: 1.125rem;
+        background: ${({ theme }) => theme.colors.secondaryText};
+      }
     }
   }
 
